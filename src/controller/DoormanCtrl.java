7282c448839fb63ff7ab7408bda48f7d6a1df580
Camelrider6 <117633367+Camelrider6@users.noreply.github.com>
2023-05-15 09:37:21 +0200

    Revert "Update .gitignore"
    This reverts commit ac0acb77c0274bd3b77f9dc80025d725e9485630.

--- src/controller/DoormanCtrl.java
+++ src/controller/DoormanCtrl.java
@@ -1,21 +1,13 @@
 package controller;
 
-<<<<<<< Updated upstream
 import java.util.Date;
 import java.util.List;
 
 import database.DataAccessException;
-=======
-import java.sql.Date;
-import java.util.List;
-import database.DataAccessException;
-
->>>>>>> Stashed changes
 import database.DoormanDAO;
 import model.Doorman;
 
 public class DoormanCtrl {
-<<<<<<< Updated upstream
 	 private DoormanDAO doormanDAO;
 
 	    public DoormanCtrl() throws DataAccessException {
@@ -24,16 +16,6 @@ public class DoormanCtrl {
 	        } catch (Exception e) {
 	        	throw new DataAccessException(e, "Unable to create doormanDAO");
 	    }
-=======
-		private DoormanDAO doormanDAO;
-
-	    public DoormanCtrl() throws DataAccessException {
-	       try {
-	    	   doormanDAO = new DoormanDAO();
-	       } catch (Exception e) {
-	    	   throw new DataAccessException(e, "Unable to create doormanDAO");
-	       }
->>>>>>> Stashed changes
 	    }
 
 	    public List<Doorman> getAvailableDoormenForShift(Date date, int barId) {
@@ -45,13 +27,7 @@ public class DoormanCtrl {
 	    }
 	    
 	    public List<Doorman> findAll() throws DataAccessException{
-<<<<<<< Updated upstream
 	    	return doormanDAO.findAll();	    	
 	    }
-=======
-	    	return doormanDAO.findAll();
-	    }
-	     
->>>>>>> Stashed changes
 	}
 
