ac0acb77c0274bd3b77f9dc80025d725e9485630
Camelrider6 <117633367+Camelrider6@users.noreply.github.com>
2023-05-15 09:30:11 +0200

    Update .gitignore

--- src/controller/DoormanCtrl.java
+++ src/controller/DoormanCtrl.java
@@ -1,13 +1,21 @@
 package controller;
 
+<<<<<<< Updated upstream
 import java.util.Date;
 import java.util.List;
 
 import database.DataAccessException;
+=======
+import java.sql.Date;
+import java.util.List;
+import database.DataAccessException;
+
+>>>>>>> Stashed changes
 import database.DoormanDAO;
 import model.Doorman;
 
 public class DoormanCtrl {
+<<<<<<< Updated upstream
 	 private DoormanDAO doormanDAO;
 
 	    public DoormanCtrl() throws DataAccessException {
@@ -16,6 +24,16 @@ public class DoormanCtrl {
 	        } catch (Exception e) {
 	        	throw new DataAccessException(e, "Unable to create doormanDAO");
 	    }
+=======
+		private DoormanDAO doormanDAO;
+
+	    public DoormanCtrl() throws DataAccessException {
+	       try {
+	    	   doormanDAO = new DoormanDAO();
+	       } catch (Exception e) {
+	    	   throw new DataAccessException(e, "Unable to create doormanDAO");
+	       }
+>>>>>>> Stashed changes
 	    }
 
 	    public List<Doorman> getAvailableDoormenForShift(Date date, int barId) {
@@ -27,7 +45,13 @@ public class DoormanCtrl {
 	    }
 	    
 	    public List<Doorman> findAll() throws DataAccessException{
+<<<<<<< Updated upstream
 	    	return doormanDAO.findAll();	    	
 	    }
+=======
+	    	return doormanDAO.findAll();
+	    }
+	     
+>>>>>>> Stashed changes
 	}
 
