14a399730236138527385fec47f0de90793b9d92
DePlaced <117630280+DePlaced@users.noreply.github.com>
2023-05-22 15:20:55 +0200

    ui fix

--- src/gui/MainCalendar.java
+++ src/gui/MainCalendar.java
@@ -67,7 +67,29 @@ public class MainCalendar extends JFrame {
 	 * @throws DataAccessException
 	 */
 	public MainCalendar() throws DataAccessException {
+		setUIStyle();
 		initComponents();
+	}
+	
+	private void setUIStyle() {
+		//https://docs.oracle.com/javase/tutorial/uiswing/lookandfeel/nimbus.html
+				//Nimbus uses Java 2D vector graphics to draw the user interface (UI), rather than static bitmaps, so the UI can be crisply rendered at any resolution
+				try {
+		            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		                if ("Nimbus".equals(info.getName())) {
+		                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+		                    break;
+		                }
+		            }
+		        } catch (ClassNotFoundException ex) {
+		            java.util.logging.Logger.getLogger(MainCalendar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		        } catch (InstantiationException ex) {
+		            java.util.logging.Logger.getLogger(MainCalendar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		        } catch (IllegalAccessException ex) {
+		            java.util.logging.Logger.getLogger(MainCalendar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+		            java.util.logging.Logger.getLogger(MainCalendar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		        }
 	}
 	
 	private void initComponents() throws DataAccessException {
@@ -118,6 +140,5 @@ public class MainCalendar extends JFrame {
 	    setLocationRelativeTo(null);
 	    pack();
 	}
-
-
+	
 }
