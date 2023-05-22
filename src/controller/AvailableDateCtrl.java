dd602a2e3635a884a58506c12558f866970d072e
Camelrider6 <117633367+Camelrider6@users.noreply.github.com>
2023-05-22 11:43:43 +0200

    Create AvailableDateCtrl.java

---
+++ src/controller/AvailableDateCtrl.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.time.LocalDate;
+
+import database.DataAccessException;
+import database.DoormanDAO;
+
+public class AvailableDateCtrl {
+private AvailableDateDAO availabledatedao;
+private LocalDate availableDate;
+private boolean isAvailable;
+
+
+
+public AvailableDateCtrl() throws DataAccessException {
+    try {
+    	availabledatedao = new AvailableDateDAO();
+    } catch (Exception e) {
+    	throw new DataAccessException(e, "Unable to create availabledateDAO");
+}
+    
+    
+}
+private LocalDate createAvailableDates(LocalDate localdate, int doormanId) throws DataAccessException {
+	return availableDate;
+	
+}
+public boolean confirmAvailability(LocalDate availableDate) {
+	return isAvailable;
+	
+}
+
+
+}
