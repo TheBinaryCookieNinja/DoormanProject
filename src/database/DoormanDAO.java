dcec6bafec7b3c04f8d4dc5afdff712090fe17cf
DePlaced <117630280+DePlaced@users.noreply.github.com>
2023-05-23 10:16:26 +0200

    Update DoormanDAO.java

--- src/database/DoormanDAO.java
+++ src/database/DoormanDAO.java
@@ -25,7 +25,8 @@ public class DoormanDAO {
 	private static final String deleteDoormanQ =
 			"delete * from Doorman where employeeId = ?";
 	private static final String getAvailableDoormenForShiftQ = 
-			findAllQ + "as d" +
+			findAllQ + 
+			"as d" +
 			"left join AvailableDates on AvailableDates.employeeId = d.employeeId" +
 			"left join DoormanWishlist on (DoormanWishList.employeeId = d.employeeId and DoormanWishlist.BarId = ?)" +
 			"left join DoormanBlacklist on (DoormanBlacklist.employeeId = d.employeeId and DoormanBlacklist.BarId = ?)" + 
