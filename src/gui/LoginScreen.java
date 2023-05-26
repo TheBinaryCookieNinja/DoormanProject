47b6c17db5fc687d5a338f2ca4b2d81d0843ba86
xdmurkch <10461252@ucn.dk>
2023-05-26 16:28:03 +0200

    update loginscreen ioexception

--- src/gui/LoginScreen.java
+++ src/gui/LoginScreen.java
@@ -36,6 +36,9 @@ public class LoginScreen extends JFrame {
 				} catch (DataAccessException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
                 loginScreen.setVisible(true);
             }
