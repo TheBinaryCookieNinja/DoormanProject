5f808cdf8269dfedf4d549cb42648a7dc60ad680
DePlaced <117630280+DePlaced@users.noreply.github.com>
2023-05-26 10:44:02 +0200

    minor changes

--- src/test/getAvailableDoormanForShiftIntegrationTest.java
+++ src/test/getAvailableDoormanForShiftIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import database.DBConnection;
 import database.DataAccessException;
 import database.DoormanDAO;
 import model.Doorman;
@@ -16,30 +17,14 @@ import java.util.List;
 public class getAvailableDoormanForShiftIntegrationTest {
 
 	private DoormanDAO doormanDAO;
-	private static final String driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-	private static final String dbName = "DMA-CSD-S221_10463670";
-	private static final String serverAddress = "hildur.ucn.dk";
-	private static final int    serverPort = 1433;
-	private static final String userName = "DMA-CSD-S221_10463670";
-	private static final String password = "Password1!";
 
 	@BeforeEach
-	public void setup() {
-			// Set up your database connection
-	        String jdbcUrl = "jdbc:mysql://localhost:3306/mydb";
-	        String username = "username";
-	        String password = "password";
-	        try {
-	            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-	            doormanRepository = new DoormanRepository(connection);
-	        } catch (SQLException e) {
-	            // Handle any setup exceptions
-	            e.printStackTrace();
-	        }
+	public void setup() throws DataAccessException {
+		doormanDAO = new DoormanDAO();
 	    }
 	
     @Test
-    public void testGetAvailableDoormenForShift() {
+    public void testGetAvailableDoormenForShiftNotEmpty() {
         // Arrange
         LocalDate date = LocalDate.of(2023, 05, 15);
         int barId = 1;
@@ -47,7 +32,7 @@ public class getAvailableDoormanForShiftIntegrationTest {
         // Act
         List<Doorman> availableDoormen;
         try {
-            availableDoormen = doormanRepository.getAvailableDoormenForShift(date, barId);
+            availableDoormen = doormanDAO.getAvailableDoormenForShift(date, barId);
         } catch (DataAccessException e) {
             // Handle any exceptions thrown during the method call
             e.printStackTrace();
@@ -58,7 +43,29 @@ public class getAvailableDoormanForShiftIntegrationTest {
         // Assert
         Assertions.assertNotNull(availableDoormen);
         Assertions.assertFalse(availableDoormen.isEmpty());
-        // Add more assertions as needed to verify the results
+        
+    }
+    
+    @Test
+    public void testGetAvailableDoormenForShiftEmpty() {
+        // Arrange
+        LocalDate date = LocalDate.of(2022, 05, 15);
+        int barId = 1;
+
+        // Act
+        List<Doorman> availableDoormen;
+        try {
+            availableDoormen = doormanDAO.getAvailableDoormenForShift(date, barId);
+        } catch (DataAccessException e) {
+            // Handle any exceptions thrown during the method call
+            e.printStackTrace();
+            Assertions.fail("Failed to retrieve available doormen for shift");
+            return;
+        }
+
+        // Assert
+        Assertions.assertNotNull(availableDoormen);
+        Assertions.assertFalse(availableDoormen.size()>0);        
     }
 }
-}
+
