17d83f3c8846a24315f9cb134145b53800690008
FrederikBRM <10463670@ucn.dk>
2023-05-26 10:17:24 +0200

    Update DoormanCtrl.java

--- src/controller/DoormanCtrl.java
+++ src/controller/DoormanCtrl.java
@@ -20,12 +20,12 @@ public class DoormanCtrl {
 	        }
 	    }
 	    
-	    public boolean getDoormanByDoormanId(int doormanId) throws DataAccessException, SQLException {
+	    public Doorman getDoormanByDoormanId(int doormanId) throws DataAccessException, SQLException {
 	       DBConnection.getInstance().startTransaction();
 	       try {
 	    	   Doorman doorman = doormanDAO.findById(doormanId);
 	    	   DBConnection.getInstance().commitTransaction();
-	    		return doorman != null;
+	    		return doorman;
 	       } catch (SQLException e) {
 	    	   DBConnection.getInstance().rollbackTransaction();
 	    	   throw new DataAccessException(e, "Unable to find doorman by ID. Please try another ID.");
@@ -33,16 +33,41 @@ public class DoormanCtrl {
 	    	
 	    }
 	    
-	    public List<Doorman> getAvailableDoormenForShift(LocalDate localDate, int barId) throws DataAccessException {
-	        return doormanDAO.getAvailableDoormenForShift(localDate, barId);
+	    public List<Doorman> getAvailableDoormenForShift(LocalDate localDate, int barId) throws DataAccessException, SQLException {
+	        DBConnection.getInstance().startTransaction();
+	        try {
+	        	List<Doorman> lDoorman = doormanDAO.getAvailableDoormenForShift(localDate, barId);
+	        	DBConnection.getInstance().commitTransaction();
+	        	return lDoorman;
+	        } catch (SQLException e) {
+	        	DBConnection.getInstance().rollbackTransaction();
+	        	throw new DataAccessException(e, "Can't find any available Doormen for shift.");
+	        }
+	    	
 	    }
 	    
-	    public List<Doorman> isDoormanOnOnWishList(int barId)throws DataAccessException {
-	        return doormanDAO.isDoormanOnWishlist(barId);
+	    public List<Doorman> isDoormanOnOnWishList(int barId)throws DataAccessException, SQLException {
+	    	DBConnection.getInstance().startTransaction();
+	    	try {
+	    		List<Doorman> lDoorman = doormanDAO.isDoormanOnWishlist(barId);
+	    		DBConnection.getInstance().commitTransaction();
+	    		return lDoorman;
+	    	} catch (SQLException e) {
+	    		DBConnection.getInstance().rollbackTransaction();
+	    		throw new DataAccessException(e, "Cant find the given doorman on a wishlist");
+	    	}
 	    }
 	    
-	    public List<Doorman> isDoormanOnAnotherShift()throws DataAccessException {
-	        return doormanDAO.isDoormanOnAnotherShift();
+	    public List<Doorman> isDoormanOnAnotherShift()throws DataAccessException, SQLException {
+	        DBConnection.getInstance().startTransaction();
+	        try {
+	        	List<Doorman> lDoorman = doormanDAO.isDoormanOnAnotherShift();
+	        	DBConnection.getInstance().commitTransaction();
+	        	return lDoorman;
+	        } catch (SQLException e) {
+	        	DBConnection.getInstance().rollbackTransaction();
+	        	throw new DataAccessException(e, "Can't find doorman on another shift.");
+	        }
 	    }
 	}
 
